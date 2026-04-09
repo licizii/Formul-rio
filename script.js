236526123c44f76d3d0b12f39ce17a41f15553b4
licizii <alicenunesferreira10@gmail.com>
2026-04-09 09:58:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function Apertar(){
+    Swal.fire({
+  title: "Cadastro Concluido!",
+  text: "Volte para a Página de login e acesse o site!",
+  icon: "success"
+});
+}
+function Clicar(){
+     Swal.fire({
+  title: "Login Concluido!",
+  text: "Acesse o site!",
+  icon: "success"
+});
+}
